7cb27e11235388efce95de5fd13ade50fd893333
epicallan <epicallan.al@gmail.com>
2016-03-27 15:28:54 +0300

    added travis support

--- app/app.py
+++ app/app.py
@@ -16,6 +16,22 @@ bash_file = 'app/deploy.sh'
 file_stats = os.stat(bash_file)
 os.chmod(bash_file, file_stats.st_mode | stat.S_IEXEC)
 
+# initialise session variables
+session['docker'] = 0
+session['travis'] = 1   # for travis status 1 means failure
+
+
+def run_bash():
+    # reset session variables
+    session['docker'] = 0
+    session['travis'] = 1
+    # run bash
+    subprocess.call(bash_file)
+
+
+def state():
+    return {'travis': session['travis'], 'docker': session['docker']}
+
 
 @app.route('/')
 def index():
@@ -23,30 +39,40 @@ def index():
     return 'Docker-Travis hook listener'
 
 
-@app.route('/set')
-def set():
-    session['key'] = 'value'
-    return 'ok'
+@app.route('/travis', methods=['POST'])
+def travis():
+    # set travis session
+    args = request.args
+    print(args)
+    # successful build has a status of 0
+    session['travis'] = args.status
+    # check if docker session is set
+    docker = session['docker']
+    if docker == 1 and session['travis'] == 0:
+        run_bash()
+    return jsonify(state())
 
 
-@app.route('/get')
-def get():
-    return session.get('key', 'not set')
+@app.route('/docker-travis', methods=['POST'])
+def docker_travis():
+    args = request.args
+    token = args.get('token')
+    travis = session['travis']
+    # print('req token: ' + token)
+    if str(token) == str(os.environ.get('TOKEN')) and travis == 0:
+        run_bash()
+    return jsonify(state()), 500
 
 
-@app.route('/docker', methods=['GET', 'POST'])
-def pong():
-    if request.method == 'POST':
-        args = request.args
-        token = args.get('token')
-        print('req token: ' + token)
-        if str(token) == str(os.environ.get('TOKEN')):
-            subprocess.call(bash_file)
-            return jsonify(success=True)
-        return jsonify(success=False), 500
-    if request.method == 'GET':
-        return "pong!"
-
+@app.route('/docker', methods=['POST'])
+def docker():
+    args = request.args
+    token = args.get('token')
+    print('req token: ' + token)
+    if str(token) == str(os.environ.get('TOKEN')):
+        subprocess.call(bash_file)
+        return jsonify(success=True)
+    return jsonify(success=False), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
